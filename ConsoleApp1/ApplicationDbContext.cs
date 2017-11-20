166c724a369030bc7384ac4a81e403e29336f57b
Calabonga <calabonga@outlook.com>
2017-11-20 18:12:49 +0700

    override for SaveChanges added

--- ConsoleApp1/ApplicationDbContext.cs
+++ ConsoleApp1/ApplicationDbContext.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
 using Calabonga.EntityFramework;
 
 namespace Calabonga.Framework.Demo
@@ -15,5 +18,39 @@ namespace Calabonga.Framework.Demo
         public SaveChangesResult LastSaveChangesResult { get; }
 
         public DbSet<Person> People { get; set; }
+
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                var createdSourceInfo = ChangeTracker.Entries().Where(e => e.State == EntityState.Added);
+                var modifiedSourceInfo = ChangeTracker.Entries().Where(e => e.State == EntityState.Modified);
+
+                foreach (var entry in createdSourceInfo)
+                {
+                    // do some staff
+                    // ...
+
+                    // Add system message
+                    LastSaveChangesResult.AddMessage($"ChangeTracker has new entities: {entry.Entity.GetType()}");
+                }
+
+                foreach (var entry in modifiedSourceInfo)
+                {
+                    // do some staff
+                    // ...
+
+                    LastSaveChangesResult.AddMessage($"ChangeTracker has modified entities: {entry.Entity.GetType()}");
+                }
+
+                return base.SaveChanges();
+            }
+            catch (DbUpdateException exception)
+            {
+                LastSaveChangesResult.Exception = exception;
+                return 0;
+            }
+        }
     }
 }
